a2a449636ee4a05941f4637551e6aff3cdaaf84b
Gabriel Lima <gabriellima_black@hotmail.com>
2021-06-20 19:04:11 -0300

    Lista simples de vacinas de COVID-19

---
+++ App1/App1/ViewModels/ItemsVacineViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.ViewModels
+{
+    public class ItemsVacineViewModel : BaseViewModel
+    {
+        public ItemsVacineViewModel()
+        {
+            Title = "Vacinas COVID-19";
+        }
+    }
+}
